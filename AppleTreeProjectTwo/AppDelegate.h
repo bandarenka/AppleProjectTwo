173ac55d151caf5f6793f685a10b2adb361030d3
Admin <admin@Mac-Admin.local>
2015-09-17 19:12:13 +0300

    Initial Commit

---
+++ AppleTreeProjectTwo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleTreeProjectTwo
+//
+//  Created by Admin on 17.09.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppleTreeProjectTwo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppleTreeProjectTwo
+//
+//  Created by Admin on 17.09.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
